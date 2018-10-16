a06382fd2ae94ecbba9c1cf348ba14c6823538a3
Stephen Lead <stephen.lead@gmail.com>
2018-10-16 15:33:11 -0300

    fix routing to individual post. remove unused variables

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Router, Route, Link } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import history from './history';
 import "./css/clean-blog.css";
 import "./css/application.css";
--- src/components/PostSnippet.js
+++ src/components/PostSnippet.js
@@ -1,19 +1,10 @@
 import React from "react";
-import Header from "./Header";
 import SanitizedHTML from "react-sanitized-html";
-import urlParse from 'url-parse'
 
 class Post extends React.Component {
 
-  getSlug = url => {
-    const parsed = urlParse(url, true);
-    return (parsed['pathname'].replace(".json", ""))
-  }
-
-  getId = id => {
-    const parsed = urlParse(id, true);
-    console.log(parsed)
-    return (parsed['pathname'].replace("/", ""))
+  setLink = url => {
+    return ("/posts/" + url)
   }
 
   render() {
@@ -23,8 +14,7 @@ class Post extends React.Component {
           <div className="col-md-12 mx-auto">
             <div className="post-preview">
               <h2 className="post-title">
-                <a href={this.getSlug(this.props.post.url)}>{this.props.post.title}</a>
-                {/*<a href={this.getId(this.props.post.id)}>{this.props.post.title}</a>*/}
+                <a href={this.setLink(this.props.post.slug)}>{this.props.post.title}</a>
               </h2>
               <SanitizedHTML className="post-subtitle"
                 allowedAttributes={{ a: ["href"] }}
