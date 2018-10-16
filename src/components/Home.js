c1fb0af171ef0b932af9bf0eeb46af614ae16541
Stephen Lead <stephen.lead@gmail.com>
2018-10-16 16:00:00 -0300

    Fetch posts in the Home component, not Posts Container

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,41 @@
 import React from "react";
+import axios from "axios";
 import Header from "./Header";
 import PostsContainer from "./PostsContainer";
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3001/posts.json?api=true")
+      .then(response => {
+        this.setState({ posts: response.data });
+      })
+      .catch(error => console.log(error));
+  }
+
+  getHeaderPhoto = () => {
+    if (this.state.posts[0] && this.state.posts[0].photos && this.state.posts[0].photos[0]) {
+      return (this.state.posts[0].photos[0].large)
+    }
+  }
+
   render() {
     return (
       <div>
-        <Header headline="Steve and Glo's travel blog" tagline="Cos working is over-rated" headerClass="subheading" />
-        <PostsContainer />
+        <Header
+          headline="Steve and Glo's travel blog"
+          tagline="Cos working is over-rated"
+          headerClass="subheading"
+          headerPhoto={this.getHeaderPhoto() || 'https://farm8.staticflickr.com/7088/7254522762_6cb4f1162c_b.jpg'}
+        />
+        <PostsContainer posts={this.state.posts}/>
       </div>
     );
   }
--- src/components/PostsContainer.js
+++ src/components/PostsContainer.js
@@ -1,30 +1,15 @@
 import React from "react";
-import axios from "axios";
+
 import PostSnippet from "./PostSnippet";
 
 class PostsContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: []
-    };
-  }
-
-  componentDidMount() {
-    axios
-      .get("http://localhost:3001/posts.json?api=true")
-      .then(response => {
-        this.setState({ posts: response.data });
-      })
-      .catch(error => console.log(error));
-  }
 
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-12">
-            {this.state.posts.map(post => {
+            {this.props.posts.map(post => {
               return (
                 <PostSnippet
                   post={post}
